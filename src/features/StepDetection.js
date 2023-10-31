864641d3d082bf4b9145f9c1c2d0195b67ca078a
ethanbg2 <ethanbg2@illinois.edu>
2023-10-30 19:02:02 -0500

    add expo pedometer

--- src/features/StepDetection.js
+++ src/features/StepDetection.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Pedometer } from "expo-sensors";
 
 export function Steps() {
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+  const subscribe = async () => {
+    return Pedometer.watchStepCount((result) => {
+      setCurrentStepCount(result.steps);
+    });
+  };
+  useEffect(() => {
+    const subscription = subscribe();
+    return () => subscription && subscription.remove();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Step Detection</Text>
+      <Text>Walk! And watch this go up: {currentStepCount}</Text>
       <StatusBar style="auto" />
     </View>
   );
